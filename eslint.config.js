9abf003ebf9f0f50ffd187202cddc2f9dc364504
abrar2030 <abrarahmedpei@gmail.com>
2025-11-24 14:22:13 +0500

    Implement updates for better performance

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+// eslint.config.js (Auto-generated)
+
+import js from "@eslint/js";
+import ts from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettier from "eslint-plugin-prettier";
+
+export default [
+  js.configs.recommended,
+
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+      }
+    },
+
+    plugins: {
+      "@typescript-eslint": ts,
+      prettier: prettier
+    },
+
+    rules: {
+      ...ts.configs.recommended.rules,
+      "prettier/prettier": "error"
+    }
+  }
+];
